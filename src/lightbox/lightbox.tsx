5ef7954e6924974e56e42a7e3a112ca76f8c1779
Lukasz Ostrowski <lukasz@ostrowski.com.pl>
2020-05-23 18:08:22 +0200

    feat: 🎸 Buttons

--- src/lightbox/lightbox.tsx
+++ src/lightbox/lightbox.tsx
@@ -1,55 +1,66 @@
-import React, { HTMLAttributes } from 'react';
-import { Modal, ModalProps } from '@material-ui/core';
+import React, { HTMLAttributes, useState } from 'react';
+import { Dialog, DialogProps, Fade, IconButton } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import { ChevronLeft, ChevronRight } from '@material-ui/icons';
 
-export interface LightboxProps extends Omit<ModalProps, 'children'> {
+export interface LightboxProps extends Omit<DialogProps, 'children'> {
   images: Array<{ src: string } & HTMLAttributes<HTMLImageElement>>;
 }
 
-const useStyles = makeStyles((theme) => ({
-  wrapper: {
-    position: 'fixed',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: '100%',
-    height: '100%',
-    maxWidth: `calc(100vw - ${theme.spacing(4)}px)`,
-    maxHeight: `calc(100vh - ${theme.spacing(4)}px)`,
-    overflow: 'hidden',
-  },
-  imageWrapper: {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    maxWidth: '100%',
-    height: '100%',
-    width: '100%',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const useStyles = makeStyles(() => ({
   image: {
     display: 'block',
-    objectFit: 'contain',
-    height: '100%',
     maxWidth: '100%',
+    maxHeight: 'calc(100vh - 64px)',
+  },
+  prevButton: {
+    position: 'absolute',
+    left: 0,
+    top: '50%',
+    transform: 'translateY(-50%)',
+  },
+  nextButton: {
+    position: 'absolute',
+    right: 0,
+    top: '50%',
+    transform: 'translateY(-50%)',
   },
 }));
 
 export const Lightbox = ({ images, ...props }: LightboxProps) => {
+  const [selected, setSelected] = useState(0);
   const styles = useStyles();
 
   return (
-    <Modal {...props}>
-      <div className={styles.wrapper}>
-        {images.map((image) => (
-          <div className={styles.imageWrapper} key={image.src}>
-            <img className={styles.image} src={image.src} />
-          </div>
-        ))}
-      </div>
-    </Modal>
+    <Dialog maxWidth="xl" {...props}>
+      {images.map((image, index) => {
+        if (index !== selected) {
+          return null;
+        }
+
+        return (
+          <Fade key={image.src} in={selected === index}>
+            <div>
+              <img className={styles.image} src={image.src} />
+            </div>
+          </Fade>
+        );
+      })}
+      <IconButton
+        color="primary"
+        className={styles.prevButton}
+        onClick={() => {
+          setSelected(selected - 1);
+        }}
+      >
+        <ChevronLeft />
+      </IconButton>
+      <IconButton
+        onClick={() => setSelected(selected + 1)}
+        className={styles.nextButton}
+      >
+        <ChevronRight />
+      </IconButton>
+    </Dialog>
   );
 };
