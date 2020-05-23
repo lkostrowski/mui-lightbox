fa70c33023fb3a083ebcd5c9e105570411801faa
Lukasz Ostrowski <lukasz@ostrowski.com.pl>
2020-05-23 19:22:26 +0200

    feat: 🎸 Format

--- example/index.tsx
+++ example/index.tsx
@@ -3,49 +3,50 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Lightbox } from '../src';
 import {
-  Button,
-  Collapse,
-  createMuiTheme,
-  CssBaseline,
-  Grow,
-  Slide,
-  ThemeProvider,
-  Typography,
+	Button,
+	Collapse,
+	createMuiTheme,
+	CssBaseline,
+	Grow,
+	Slide,
+	ThemeProvider,
+	Typography,
 } from '@material-ui/core';
 import { useState } from 'react';
 
 const theme = createMuiTheme();
 
 const App = () => {
-  const [open, setOpen] = useState(true);
+	const [open, setOpen] = useState(true);
 
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Typography variant="h3">Lightbox Demo</Typography>
-      <Button color="primary" variant="contained" onClick={() => setOpen(true)}>
-        Open default
-      </Button>
-      <Lightbox
-        onClose={() => setOpen(false)}
-        images={[
-          {
-            src:
-              'https://images.unsplash.com/photo-1590157678696-a5151f512cbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2251&q=80',
-          },
-          {
-            src:
-              'https://images.unsplash.com/photo-1587613990174-1f14ba3be7cb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80',
-          },
-          {
-            src:
-              'https://images.unsplash.com/photo-1590114025443-530951ab7702?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80',
-          },
-        ]}
-        open={open}
-      />
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<Typography variant="h3">Lightbox Demo</Typography>
+			<Button color="primary" variant="contained" onClick={() => setOpen(true)}>
+				Open default
+			</Button>
+			<Lightbox
+				onClose={() => setOpen(false)}
+				images={[
+					{
+						title: 'Some image title',
+						src:
+							'https://images.unsplash.com/photo-1590157678696-a5151f512cbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2251&q=80',
+					},
+					{
+						src:
+							'https://images.unsplash.com/photo-1587613990174-1f14ba3be7cb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80',
+					},
+					{
+						src:
+							'https://images.unsplash.com/photo-1590114025443-530951ab7702?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80',
+					},
+				]}
+				open={open}
+			/>
+		</ThemeProvider>
+	);
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/lightbox/lightbox.tsx
+++ src/lightbox/lightbox.tsx
@@ -1,112 +1,109 @@
-import React, {
-  HTMLAttributes,
-  useState,
-  useCallback,
-  KeyboardEventHandler,
-} from 'react';
+import React, { HTMLAttributes, useState, useCallback, KeyboardEventHandler } from 'react';
 import {
-  Dialog,
-  DialogProps,
-  Fade,
-  Grow,
-  IconButton,
-  Slide,
-  Zoom,
+	Card,
+	Dialog,
+	DialogProps,
+	Fade,
+	Grow,
+	IconButton,
+	Slide,
+	Typography,
+	Zoom,
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { ChevronLeft, ChevronRight } from '@material-ui/icons';
 import Collapse from '@material-ui/core/Collapse';
 
 export interface LightboxProps extends Omit<DialogProps, 'children'> {
-  images: Array<{ src: string } & HTMLAttributes<HTMLImageElement>>;
-  TransitionComponent?:
-    | typeof Collapse
-    | typeof Fade
-    | typeof Slide
-    | typeof Grow
-    | typeof Zoom;
+	images: Array<{ src: string } & HTMLAttributes<HTMLImageElement>>;
+	TransitionComponent?: typeof Collapse | typeof Fade | typeof Slide | typeof Grow | typeof Zoom;
 }
 
-const useStyles = makeStyles(() => ({
-  image: {
-    display: 'block',
-    maxWidth: '100%',
-    maxHeight: 'calc(100vh - 64px)',
-  },
-  prevButton: {
-    position: 'absolute',
-    left: 0,
-    top: '50%',
-    transform: 'translateY(-50%)',
-  },
-  nextButton: {
-    position: 'absolute',
-    right: 0,
-    top: '50%',
-    transform: 'translateY(-50%)',
-  },
+const useStyles = makeStyles((theme) => ({
+	image: {
+		display: 'block',
+		maxWidth: '100%',
+		maxHeight: `calc(100vh - ${theme.spacing(8)}px)`,
+	},
+	prevButton: {
+		position: 'absolute',
+		left: 0,
+		top: '50%',
+		transform: 'translateY(-50%)',
+	},
+	nextButton: {
+		position: 'absolute',
+		right: 0,
+		top: '50%',
+		transform: 'translateY(-50%)',
+	},
+	titleCard: {
+		position: 'absolute',
+		bottom: theme.spacing(4),
+		padding: theme.spacing(2),
+		left: '50%',
+		transform: 'translateX(-50%)',
+	},
 }));
 
 export const Lightbox = ({
-  images,
-  TransitionComponent = Fade,
-  onKeyDown,
-  ...props
+	images,
+	TransitionComponent = Fade,
+	onKeyDown,
+	...props
 }: LightboxProps) => {
-  const [selected, setSelected] = useState(0);
-  const styles = useStyles();
+	const [selected, setSelected] = useState(0);
+	const styles = useStyles();
 
-  const handlePreviousImageRequest = useCallback(() => {
-    setSelected((sel) => (sel + images.length - 1) % images.length);
-  }, [images]);
+	const handlePreviousImageRequest = useCallback(() => {
+		setSelected((sel) => (sel + images.length - 1) % images.length);
+	}, [images]);
 
-  const handleNextImageRequest = useCallback(() => {
-    setSelected((sel) => (sel + 1) % images.length);
-  }, [images]);
+	const handleNextImageRequest = useCallback(() => {
+		setSelected((sel) => (sel + 1) % images.length);
+	}, [images]);
 
-  const handleKeyDown = useCallback<KeyboardEventHandler<HTMLDivElement>>(
-    (e) => {
-      switch (e.which) {
-        case 37:
-          handlePreviousImageRequest();
-          onKeyDown && onKeyDown(e);
-          break;
-        case 39:
-          handleNextImageRequest();
-          onKeyDown && onKeyDown(e);
-          break;
-        default:
-          onKeyDown && onKeyDown(e);
-      }
-    },
-    []
-  );
+	const handleKeyDown = useCallback<KeyboardEventHandler<HTMLDivElement>>((e) => {
+		switch (e.which) {
+			case 37:
+				handlePreviousImageRequest();
+				onKeyDown && onKeyDown(e);
+				break;
+			case 39:
+				handleNextImageRequest();
+				onKeyDown && onKeyDown(e);
+				break;
+			default:
+				onKeyDown && onKeyDown(e);
+		}
+	}, []);
 
-  return (
-    <Dialog maxWidth="xl" {...props} onKeyDown={handleKeyDown}>
-      {images.map((image, index) => {
-        if (index !== selected) {
-          return null;
-        }
+	return (
+		<Dialog maxWidth="xl" {...props} onKeyDown={handleKeyDown}>
+			{images.map((image, index) => {
+				if (index !== selected) {
+					return null;
+				}
 
-        return (
-          <TransitionComponent key={image.src} in={selected === index}>
-            <img className={styles.image} src={image.src} />
-          </TransitionComponent>
-        );
-      })}
-      <IconButton
-        className={styles.prevButton}
-        onClick={handlePreviousImageRequest}
-      >
-        <ChevronLeft />
-      </IconButton>
-      <IconButton
-        onClick={handleNextImageRequest}
-        className={styles.nextButton}
-      >
-        <ChevronRight />
-      </IconButton>
-    </Dialog>
-  );
+				return (
+					<TransitionComponent key={image.src} in={selected === index}>
+						<div>
+							<img className={styles.image} src={image.src} />
+							{image.title && (
+								<Card className={styles.titleCard} variant="outlined">
+									<Typography variant="caption">{image.title}</Typography>
+								</Card>
+							)}
+						</div>
+					</TransitionComponent>
+				);
+			})}
+			<IconButton className={styles.prevButton} onClick={handlePreviousImageRequest}>
+				<ChevronLeft />
+			</IconButton>
+			<IconButton onClick={handleNextImageRequest} className={styles.nextButton}>
+				<ChevronRight />
+			</IconButton>
+		</Dialog>
+	);
 };
