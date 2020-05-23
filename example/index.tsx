23dc78db8043323d61edd9f0b0ab26a15588d1a5
Lukasz Ostrowski <lukasz@ostrowski.com.pl>
2020-05-23 18:51:41 +0200

    feat: 🎸 Transitions

--- example/index.tsx
+++ example/index.tsx
@@ -2,7 +2,16 @@ import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Lightbox } from '../src';
-import { createMuiTheme, CssBaseline, ThemeProvider } from '@material-ui/core';
+import {
+  Button,
+  Collapse,
+  createMuiTheme,
+  CssBaseline,
+  Grow,
+  Slide,
+  ThemeProvider,
+  Typography,
+} from '@material-ui/core';
 import { useState } from 'react';
 
 const theme = createMuiTheme();
@@ -13,6 +22,10 @@ const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <Typography variant="h3">Lightbox Demo</Typography>
+      <Button color="primary" variant="contained" onClick={() => setOpen(true)}>
+        Open default
+      </Button>
       <Lightbox
         onClose={() => setOpen(false)}
         images={[
@@ -23,6 +36,10 @@ const App = () => {
           {
             src:
               'https://images.unsplash.com/photo-1587613990174-1f14ba3be7cb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80',
+          },
+          {
+            src:
+              'https://images.unsplash.com/photo-1590114025443-530951ab7702?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80',
           },
         ]}
         open={open}
--- src/lightbox/lightbox.tsx
+++ src/lightbox/lightbox.tsx
@@ -1,10 +1,30 @@
-import React, { HTMLAttributes, useState } from 'react';
-import { Dialog, DialogProps, Fade, IconButton } from '@material-ui/core';
+import React, {
+  HTMLAttributes,
+  useState,
+  useCallback,
+  KeyboardEventHandler,
+} from 'react';
+import {
+  Dialog,
+  DialogProps,
+  Fade,
+  Grow,
+  IconButton,
+  Slide,
+  Zoom,
+} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { ChevronLeft, ChevronRight } from '@material-ui/icons';
+import Collapse from '@material-ui/core/Collapse';
 
 export interface LightboxProps extends Omit<DialogProps, 'children'> {
   images: Array<{ src: string } & HTMLAttributes<HTMLImageElement>>;
+  TransitionComponent?:
+    | typeof Collapse
+    | typeof Fade
+    | typeof Slide
+    | typeof Grow
+    | typeof Zoom;
 }
 
 const useStyles = makeStyles(() => ({
@@ -27,36 +47,62 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
-export const Lightbox = ({ images, ...props }: LightboxProps) => {
+export const Lightbox = ({
+  images,
+  TransitionComponent = Fade,
+  onKeyDown,
+  ...props
+}: LightboxProps) => {
   const [selected, setSelected] = useState(0);
   const styles = useStyles();
 
+  const handlePreviousImageRequest = useCallback(() => {
+    setSelected((sel) => (sel + images.length - 1) % images.length);
+  }, [images]);
+
+  const handleNextImageRequest = useCallback(() => {
+    setSelected((sel) => (sel + 1) % images.length);
+  }, [images]);
+
+  const handleKeyDown = useCallback<KeyboardEventHandler<HTMLDivElement>>(
+    (e) => {
+      switch (e.which) {
+        case 37:
+          handlePreviousImageRequest();
+          onKeyDown && onKeyDown(e);
+          break;
+        case 39:
+          handleNextImageRequest();
+          onKeyDown && onKeyDown(e);
+          break;
+        default:
+          onKeyDown && onKeyDown(e);
+      }
+    },
+    []
+  );
+
   return (
-    <Dialog maxWidth="xl" {...props}>
+    <Dialog maxWidth="xl" {...props} onKeyDown={handleKeyDown}>
       {images.map((image, index) => {
         if (index !== selected) {
           return null;
         }
 
         return (
-          <Fade key={image.src} in={selected === index}>
-            <div>
-              <img className={styles.image} src={image.src} />
-            </div>
-          </Fade>
+          <TransitionComponent key={image.src} in={selected === index}>
+            <img className={styles.image} src={image.src} />
+          </TransitionComponent>
         );
       })}
       <IconButton
-        color="primary"
         className={styles.prevButton}
-        onClick={() => {
-          setSelected(selected - 1);
-        }}
+        onClick={handlePreviousImageRequest}
       >
         <ChevronLeft />
       </IconButton>
       <IconButton
-        onClick={() => setSelected(selected + 1)}
+        onClick={handleNextImageRequest}
         className={styles.nextButton}
       >
         <ChevronRight />
